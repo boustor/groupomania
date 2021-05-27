8c84863762d6a1e57cc3eb3b60382e51b4639035
boustor <boustor.deb@gmail.com>
2021-05-27 12:09:40 +0200

    Début front

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavLink from '@/components/Navlink.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    NavLink
+  }
+}
+</script>
