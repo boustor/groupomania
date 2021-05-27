fea033bb5800f7ef1012ee084bebb786263ee8b3
boustor <boustor.deb@gmail.com>
2021-05-27 12:09:33 +0200

    Début front

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     HOST: process.env.SQL_HOST,
+    PORT: process.env.SQL_PORT,
     USER: process.env.SQL_USER,
     PASSWORD: process.env.SQL_PSWD,
     DB: process.env.SQL_BASE,
-    dialect: "mysql2",
-  };
+    dialect: "mysql",
+};
--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -2,9 +2,10 @@ const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
+    host: dbConfig.HOST,
+    port: dbConfig.PORT,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
 });
 
 const db = {};
