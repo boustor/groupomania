e630e8dc031d8e3bba3fde80fb09f4187afb1c61
boustor <boustor.dev@gmail.com>
2021-09-04 22:51:59 +0200

    debut gestion image

---
+++ frontend/src/components/AffCommentaire.vue
@@ -0,0 +1,146 @@
+<template>
+  <div>
+    <h3 class="commentaire" v-if="isCom">Commentaire(s)</h3>
+    <div v-for="message in listes" :key="message.id" class="cadreMessage">
+      <div class="enteteMessage">
+        <div>message de : {{ message.nom }}</div>
+        <div>le : {{ dateTime(message.createdAt) }}</div>
+      </div>
+      <div class="objetMessageCom">{{ message.objet }}</div>
+      <textarea v-model="message.message" class="messageAreaCom"></textarea>
+
+      <div class="fondCommentaire">
+        <div>
+          <router-link :to="{ name: 'Commentaire', params: { id_mess: message.id_mess, id: message.id } }"
+            v-if="userCtrl.userId == message.id_usr">
+            <font-awesome-icon :icon="['fas', 'edit']" />
+          </router-link>
+          &nbsp;
+          &nbsp;
+
+          <span class="butSupprimer" v-on:click="supprimerMessage(message.id)" v-if="butSupprimer">
+            <font-awesome-icon :icon="['fas', 'trash']" />
+          </span>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="alert-mask" v-if="isAlert">
+    <div class="alert-wrapper">
+      <div class="alert-container">
+        <div class="alert-body">
+          Message supprimé
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import moment from "moment";
+  import webToken from 'jsonwebtoken'
+
+  export default {
+    name: "AffCommentaire",
+    props: ['idmessage'],
+    data() {
+      return {
+        listes: [],
+        isCom: false,
+        butSupprimer: null,
+        isAlert: false,
+      }
+    },
+    methods: {
+      dateTime(value) {
+        return moment(value).format("DD-MM-YYYY");
+      },
+      affSupprimer: function () {
+        if (this.userCtrl.admin == true) {
+          this.butSupprimer = true;
+        }
+      },
+      ListeCommentaire: function () {
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json"
+          },
+        };
+        fetch("http://localhost:3000/api/commentaires/all/" + this.idmessage, requestOptions)
+          .then((listes) => listes.json())
+          .then((listes) => {
+            if (listes == '') {
+              console.log('rien')
+            } else {
+              this.isCom = 'true';
+              this.listes = listes
+            }
+          });
+      },
+      // ---------- On supprimer le message ----------
+      supprimerMessage: function (id) {
+        const token = localStorage.getItem("userToken");
+        if (!token) {
+          this.$router.push("/");
+        }
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        };
+
+        fetch("http://localhost:3000/api/commentaires/supprimer/" + id, requestOptions)
+          .then((listes) => listes.json())
+          .then((listes) => {
+            if (!listes || listes.messErr == "Etoken") {
+              this.$router.push("/");
+            } else {
+              this.isAlert = true;
+              setTimeout(() => { this.isAlert = false }, 800);
+              this.$router.go();
+            }
+          });
+      }
+    },
+    mounted() {
+      this.userCtrl = webToken.decode(localStorage.getItem("userToken"), process.env.TOKEN_KEY);
+      this.affSupprimer();
+      this.ListeCommentaire();
+    }
+  }
+</script>
+
+<style>
+  .fondCommentaire {
+    height: 35px;
+    border: 1px solid black;
+    border-radius: 0 0 5px 5px;
+    text-align: right;
+    padding-top: 5px;
+    padding-right: 15px;
+    padding-left: 15px;
+  }
+
+  .messageAreaCom {
+    width: 95%;
+    height: 100px;
+    background-color: #82bdf5;
+    color: white;
+    border: 0;
+    resize: none;
+    border-radius: 5px;
+  }
+
+  .objetMessageCom {
+    width: 95%;
+    margin: auto;
+    margin-bottom: 5px;
+    border-radius: 5px;
+    background-color: #0e91fc;
+  }
+</style>
---
+++ frontend/src/views/Commentaire.vue
@@ -0,0 +1,175 @@
+<template>
+  <br />
+  <div class="d-flex justify-content-center">
+    <div class="
+        d-flex
+        justify-content-center
+        border border-1
+        shadow-lg
+        col-sm-6
+        rounded
+        bg-white
+      ">
+      <div class="col-sm-12">
+        <h3 class="mb-3 my-4">Commantaire</h3>
+
+        <div class="my-4 px-3">
+          <label for="email" class="form-label">Objet</label>
+          <div class="input-group">
+            <input type="text" class="form-control" id="objet" aria-describedby="text" v-model="objet" />
+          </div>
+        </div>
+
+        <div class="my-4 px-3">
+          <label for="password" class="form-label">Message</label>
+          <div class="input-group">
+            <textarea class="form-control text-break text-lg-start" id="message" v-model="message"></textarea>
+          </div>
+        </div>
+
+        <div class="my-4 px-3">
+          <div class="mb-3">
+            <label for="formFile" class="form-label">Charger une image</label>
+            <input class="form-control" id="fileimg" type="file" @change="viewFile" accept="image/jpeg, image/jpg"
+              title="Format jpeg, jpg">
+          </div>
+        </div>
+
+        <div id="preview">
+          <img v-if="imageView" :src="imageView" class="affichageImage" />
+        </div>
+        <div class="positionBouton">
+          <div class="cadragebouton">
+            <button class="bouton" v-on:click="supprimerMessage(id)">Supprimer</button>
+          </div>
+          <div class="cadragebouton">
+            <button class="bouton" v-on:click="validerMessage()">Valider</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="alert-mask" v-if="isAlert">
+    <div class="alert-wrapper">
+      <div class="alert-container">
+        <div class="alert-body">
+          Message supprimé
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Commentaire",
+    props: ["id_mess", "id"],
+    data() {
+      return {
+        objet: null,
+        message: null,
+        id_usr: null,
+        fileimg: null,
+        imageView: null,
+        isMessage: null,
+        isAlert: false,
+      };
+    },
+    methods: {
+      viewFile: function (file) {
+        const image = file.target.files[0];
+        this.imageView = URL.createObjectURL(image);
+      },
+      affmessage: function () {
+
+      },
+      // ---------- on recherche un message ----------
+      rechercheMessage: function () {
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("userToken"),
+          },
+        };
+        fetch("http://localhost:3000/api/commentaires/" + this.id, requestOptions)
+          .then((message) => message.json())
+          .then((message) => {
+            if (message.messErr == "Etoken") {
+              this.$router.push("/");
+            }
+            if (!message) {
+              this.$router.push("/listeMessages");
+            }
+            this.objet = message.objet;
+            this.message = message.message;
+          });
+      },
+      // ---------- on ecrit le message ----------
+      validerMessage: function () {
+        const token = localStorage.getItem("userToken");
+        //const formData = new FormData(),
+        const requestOptions = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+
+          //formData.append('fileImg', this.imageView),
+          body: JSON.stringify({
+            id: this.id,
+            id_mess: this.id_mess,
+            objet: this.objet,
+            message: this.message,
+          }),
+        };
+        fetch("http://localhost:3000/api/commentaires/commentaire", requestOptions)
+          .then((message) => message.json())
+          .then((message) => {
+            if (message.messErr == "Etoken") {
+              this.$router.push("/");
+            }
+            this.$router.push('/listeMessages')
+          });
+      },
+      // ---------- On supprimer le message ----------
+      supprimerMessage: function (id) {
+        const token = localStorage.getItem("userToken");
+        if (!token) {
+          this.$router.push("/");
+        }
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        };
+
+        fetch("http://localhost:3000/api/commentaires/supprimer/" + id, requestOptions)
+          .then((listes) => listes.json())
+          .then((listes) => {
+            if (!listes || listes.messErr == "Etoken") {
+              this.$router.push("/");
+            } else {
+              this.isAlert = true;
+              setTimeout(() => { this.isAlert = false }, 800);
+              this.$router.push('/listeMessages')
+            }
+          });
+      },
+    },
+    mounted() {
+      if (this.id != 0) this.rechercheMessage();
+    },
+  };
+</script>
+
+<style>
+  .affichageImage {
+    width: 500px;
+    padding: 5px;
+  }
+</style>
